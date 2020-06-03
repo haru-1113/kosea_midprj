bdda2e5993f923dacbc493159de883dd9befd0d7
user <user@DESKTOP-N9O301I>
2020-06-03 16:32:17 +0900

    Third Commit

--- SingleProject/src/display/Display.java
+++ SingleProject/src/display/Display.java
@@ -7,18 +7,15 @@ public class Display {
 	static Frame f;
 	static Dialog dl;
 	static Choice cho;
+
 	public static void main(String[] args) {
 		f = new Frame();
 		f.setSize(900, 700);
 		f.setLayout(null);
-		
-		
-		
-		
-		
-		
+		System.out.println("콘솔창 출력 메서드");
+
 		f.setVisible(true);
-		
+
 	}
 
 }
