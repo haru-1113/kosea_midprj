107a596f0836f2823236160f72be3dd01ee82d24
user <user@DESKTOP-N9O301I>
2020-06-03 16:17:40 +0900

    Initial commit

---
+++ SingleProject/src/display/Display.java
@@ -0,0 +1,24 @@
+package display;
+
+import java.awt.*;
+
+public class Display {
+
+	static Frame f;
+	static Dialog dl;
+	static Choice cho;
+	public static void main(String[] args) {
+		f = new Frame();
+		f.setSize(900, 700);
+		f.setLayout(null);
+		
+		
+		
+		
+		
+		
+		f.setVisible(true);
+		
+	}
+
+}
